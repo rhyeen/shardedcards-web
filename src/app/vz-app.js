2760da5c7efb1868e59377ad9f7ae59667816158
Ryan Saunders <contact.ryan.saunders@gmail.com>
2018-05-18 08:16:45 -0600

    init

---
+++ src/app/vz-app.js
@@ -0,0 +1,146 @@
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+
+class VzApp extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          --app-primary-color: #4285f4;
+          --app-secondary-color: black;
+
+          display: block;
+        }
+
+        iron-pages {
+          height: 100%;
+        }
+
+        app-drawer-layout:not([narrow]) [drawer-toggle] {
+          display: none;
+        }
+
+        app-header {
+          color: #fff;
+          background-color: var(--app-primary-color);
+        }
+
+        app-header paper-icon-button {
+          --paper-icon-button-ink-color: white;
+        }
+
+        .drawer-list {
+          margin: 0 20px;
+        }
+
+        .drawer-list a {
+          display: block;
+          padding: 0 16px;
+          text-decoration: none;
+          color: var(--app-secondary-color);
+          line-height: 40px;
+        }
+
+        .drawer-list a.iron-selected {
+          color: black;
+          font-weight: bold;
+        }
+      </style>
+
+      <app-location route="{{route}}" url-space-regex="^[[rootPath]]">
+      </app-location>
+
+      <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
+      </app-route>
+      
+      <app-drawer-layout fullbleed="" narrow="{{narrow}}">
+        <!-- Drawer content -->
+        <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
+          <app-toolbar>Menu</app-toolbar>
+          <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
+            <a name="componentEditor" href="[[rootPath]]componentEditor">View One</a>
+            <a name="view2" href="[[rootPath]]view2">View Two</a>
+            <a name="view3" href="[[rootPath]]view3">View Three</a>
+          </iron-selector>
+        </app-drawer>
+
+        <!-- Main content -->
+        <app-header-layout has-scrolling-region="">
+
+          <app-header slot="header" condenses="" reveals="" effects="waterfall">
+            <app-toolbar>
+              <paper-icon-button icon="vz-icons:menu" drawer-toggle=""></paper-icon-button>
+              <div main-title="">Vizeb</div>
+            </app-toolbar>
+          </app-header>
+
+          <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+            <vz-view-component-editor name="componentEditor"></vz-view-component-editor>
+            <vz-view2 name="view2"></vz-view2>
+            <vz-view3 name="view3"></vz-view3>
+            <vz-view404 name="view404"></vz-view404>
+          </iron-pages>
+        </app-header-layout>
+      </app-drawer-layout>
+    `;
+  }
+
+  static get properties() {
+    return {
+      page: {
+        type: String,
+        reflectToAttribute: true,
+        observer: '_pageChanged'
+      },
+      routeData: Object,
+      subroute: Object
+    };
+  }
+
+  static get observers() {
+    return [
+      '_routePageChanged(routeData.page)'
+    ];
+  }
+
+  _routePageChanged(page) {
+    // Show the corresponding page according to the route.
+    //
+    // If no page was found in the route data, page will be an empty string.
+    // Show 'componentEditor' in that case. And if the page doesn't exist, show 'view404'.
+    if (!page) {
+      this.page = 'componentEditor';
+    } else if (['componentEditor', 'view2', 'view3'].indexOf(page) !== -1) {
+      this.page = page;
+    } else {
+      this.page = 'view404';
+    }
+
+    // Close a non-persistent drawer when the page & route are changed.
+    if (!this.$.drawer.persistent) {
+      this.$.drawer.close();
+    }
+  }
+
+  _pageChanged(page) {
+    // Import the page component on demand.
+    //
+    // Note: `polymer build` doesn't like string concatenation in the import
+    // statement, so break it up.
+    switch (page) {
+      case 'componentEditor':
+        import('./views/vz-view-component-editor.js');
+        break;
+      case 'view2':
+        import('./views/vz-view2.js');
+        break;
+      case 'view3':
+        import('./views/vz-view3.js');
+        break;
+      case 'view404':
+        import('./views/vz-view404.js');
+        break;
+    }
+  }
+}
+
+window.customElements.define('vz-app', VzApp);
---
+++ src/vizeb-app.js
@@ -0,0 +1,35 @@
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
+import '@polymer/app-layout/app-drawer/app-drawer.js';
+import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
+import '@polymer/app-layout/app-header/app-header.js';
+import '@polymer/app-layout/app-header-layout/app-header-layout.js';
+import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
+import '@polymer/app-layout/app-toolbar/app-toolbar.js';
+import '@polymer/app-route/app-location.js';
+import '@polymer/app-route/app-route.js';
+import '@polymer/iron-pages/iron-pages.js';
+import '@polymer/iron-selector/iron-selector.js';
+import '@polymer/paper-icon-button/paper-icon-button.js';
+import '@polymer/paper-input/paper-input.js';
+import './globals/vz-icons.js';
+import './globals/shared-styles.js';
+import './app/vz-app.js';
+
+// Gesture events like tap and track generated from touch will not be
+// preventable, allowing for better scrolling performance.
+setPassiveTouchGestures(true);
+
+// Set Polymer's root path to the same value we passed to our service worker
+// in `index.html`.
+setRootPath(VizebAppGlobals.rootPath);
+
+class VizebApp extends PolymerElement {
+  static get template() {
+    return html`
+      <vz-app>Loading...</vz-app>
+    `;
+  }
+}
+
+window.customElements.define('vizeb-app', VizebApp);
